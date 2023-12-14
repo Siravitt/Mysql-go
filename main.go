ed5e542af0904180834e86ee3d0a18f2bdcf27fe
A667450 A667450 <a667450@AR8040667450N1.home>
2023-12-14 16:13:20 +0700

    :art:

--- main.go
+++ main.go
@@ -12,18 +12,31 @@ type Animal struct {
 	Name string
 }
 
+var db *sql.DB
+
 func main() {
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/test_connection_go")
+	var err error
+	db, err = sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/test_connection_go")
 
 	if err != nil {
 		panic(err)
 	}
-	if err = db.Ping(); err != nil {
-		panic(err)
+
+	animals, err := GetAnimals()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(animals)
+}
+
+func GetAnimals() ([]Animal, error) {
+	if err := db.Ping(); err != nil {
+		return nil, err
 	}
 
-	if err = db.Ping(); err != nil {
-		panic(err)
+	if err := db.Ping(); err != nil {
+		return nil, err
 	}
 
 	query := "select id, name from animal"
@@ -31,7 +44,7 @@ func main() {
 	rows, err := db.Query(query)
 
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	defer rows.Close()
@@ -42,10 +55,9 @@ func main() {
 		animal := Animal{}
 		err = rows.Scan(&animal.Id, &animal.Name)
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 		animals = append(animals, animal)
 	}
-
-	fmt.Println(animals)
+	return animals, nil
 }
